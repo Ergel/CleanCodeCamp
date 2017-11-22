739cd446056b8db59e19035cece68c0f078c9e64
Ergel <fuzuli_99@yahoo.com>
2017-11-22 09:51:37 +0100

    Erarbeitete Lösung ind er CCC Gruppe

--- FizzBuzz/FizzBuzz/Class1.cs
+++ FizzBuzz/FizzBuzz/Class1.cs
@@ -1,6 +1,58 @@
-﻿namespace CodeKata.FizzBuzz
+﻿using System;
+using System.Text;
+
+namespace CodeKata.FizzBuzz
 {
     public class Class1
     {
+        public static string FizzBuzzStringHolen(int input)
+        {
+
+            if (input % 15 == 0)
+            {
+                return "FizzBuzz";
+            }
+
+            if (input % 3 == 0)
+            {
+                return "Fizz";
+            }
+
+            if (input % 5 == 0)
+            {
+                return "Buzz";
+            }
+
+            return FizzBuzzContoint3oder5StringHolen(input);
+        }
+
+        private static string FizzBuzzContoint3oder5StringHolen(int input)
+        {
+            var wert = input.ToString();
+
+            if (wert.Contains("3"))
+            {
+                wert = "Fizz";
+            }
+
+            if (wert.Contains("5"))
+            {
+                return "Buzz";
+            }
+
+            return wert;
+        }
+
+        public static string Ausgabe1Bis100()
+        {
+            var uebersetzteStringBuilder = new StringBuilder();
+            for (int input = 1; input <= 100; input++)
+            {
+                uebersetzteStringBuilder.Append(FizzBuzzStringHolen(input));
+                uebersetzteStringBuilder.Append(" ");
+            }
+
+            return uebersetzteStringBuilder.ToString(0, uebersetzteStringBuilder.Length - 1);
+        }
     }
 }
--- FizzBuzz/Test.FizzBuzz/Class1Test.cs
+++ FizzBuzz/Test.FizzBuzz/Class1Test.cs
@@ -1,10 +1,37 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Linq;
+using CodeKata.FizzBuzz;
+using NUnit.Framework;
 
 namespace CodeKata.Test.FizzBuzz
 {
     [TestFixture]
     public class Class1Test
     {
+        [Test]
+        [TestCase(1, "1")]
+        [TestCase(3, "Fizz")]
+        [TestCase(13, "Fizz")]
+        [TestCase(31, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(52, "Buzz")]
+        public void TestFizzBuzzStringHolen(int input, string output)
+        {
+            var uebersetzteString = Class1.FizzBuzzStringHolen(input);
+            Assert.That(uebersetzteString, Is.EqualTo(output));
+        }
 
+        [Test]
+        public void TesteAusgabe1Bis100()
+        {
+            var uebersetzteString = Class1.Ausgabe1Bis100();
+            Assert.That(uebersetzteString.StartsWith("1 2 Fizz 4 Buzz Fizz 7"), Is.True);
+
+            var anzahlDerLeerzeichen = uebersetzteString.Count(chr => chr == ' ');
+            Assert.That(anzahlDerLeerzeichen, Is.EqualTo(99));
+
+            Console.Out.Write(uebersetzteString);
+        }
     }
 }
